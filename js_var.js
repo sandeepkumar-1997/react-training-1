6b6d7bef499be657735448ad4c72081cdcb5edfa
Sandeep Kumar <sandeep.kumar@softobiz.com>
2021-05-25 13:34:05 +0530

    Javascript variables, operators, comments

--- js_var.js
+++ js_var.js
@@ -2,7 +2,7 @@ let name = "Sandeep";                           // Using double quotes
 let str = "It's 'alright'.";                   // Single quote inside double quotes
 let score1 = 80;                               // assignment operator (=) assigns a value to a variable
 let score2 = 52.70;
-let car;                            
+let animal;                            
 let sum = score1 + score2;                     // addition operator (+) adds numbers
 let sum2 = name + score1;
 let sum3 = score1+ score2 + name;
@@ -15,4 +15,4 @@ console.log("Sum of the scores: " +sum);
 console.log(sum2);
 console.log(sum3);
 console.log(sum4);
-console.log(car);                               // Value is undefined, type is undefined 
+console.log(animal);                               // Value is undefined, type is undefined 
