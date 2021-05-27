23f4a19aa75370dcfe3766db59582cb098507d1e
Sandeep Kumar <sandeep.kumar@softobiz.com>
2021-05-27 09:56:02 +0530

    Array Javascript programs

---
+++ array.js
@@ -0,0 +1,9 @@
+let animal = ["Dog", "Cat", "Cow", "Goat", "Lion"];
+console.log(animal[1]);                     // access elements of an array using index number
+console.log(animal);
+console.log(animal.length);                 // get length of the array
+let lastElement = animal[animal.length - 1];
+console.log(lastElement);                   // access last element of an array
+let newAnimal = animal.push("Elephant");    // push method adds new items to the end of an array
+console.log(newAnimal);                     // and returns the new length
+console.log(animal);
