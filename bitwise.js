9ff84fe4f4e65ea7e0b7f3f6a0479cf04a468aa2
Sandeep Kumar <sandeep.kumar@softobiz.com>
2021-05-31 10:06:28 +0530

    Bitwise Operator In JS

---
+++ bitwise.js
@@ -0,0 +1,12 @@
+let x = 5 & 1;          // Bitwise AND
+console.log(x);
+x = 5 | 1;              // Bitwise OR
+console.log(x);
+x = 5 ^ 1;              // Bitwise XOR
+console.log(x);
+x = ~5;                 // Bitwise NOT
+console.log(x);
+x = 5 << 2;             // Bitwise Left Shift
+console.log(x);
+x = -5 >> 1;            // Bitwise Right Shift
+console.log(x);
